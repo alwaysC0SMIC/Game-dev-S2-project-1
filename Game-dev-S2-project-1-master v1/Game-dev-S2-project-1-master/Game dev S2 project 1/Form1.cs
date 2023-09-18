499078509dfb54f517589c3f7da03718786ebb0b
MaximusPrime29 <142296959+MaximusPrime29@users.noreply.github.com>
2023-09-18 17:46:52 +0200

    Add files via upload
    Checked comments

---
+++ Game-dev-S2-project-1-master v1/Game-dev-S2-project-1-master/Game dev S2 project 1/Form1.cs
@@ -0,0 +1,74 @@
+using System.Drawing.Text;
+
+namespace Game_dev_S2_project_1
+{
+    public partial class Form1 : Form
+    {
+        private GameEngine gameEngine;
+        public Level.Direction keyPress = Level.Direction.None;
+        public const int NUMBER_OF_LEVELS = 10;
+
+        // private GameEngine field
+        //Initialises the GameEngine field in the Form’s constructor and set the 
+        //number-of-levels parameter
+        public Form1()
+        {
+            InitializeComponent();
+            gameEngine = new GameEngine(NUMBER_OF_LEVELS);
+            UpdateDisplay();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        // Assigns GameEngine field’s ToString result to your display label’s text property
+        public void UpdateDisplay()
+        {
+            IbIDisplay.Text = gameEngine.ToString();
+        }
+
+        //The label
+        private void IbIDisplay_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+
+            keyPress = Level.Direction.Up;
+            gameEngine.TriggerMovement(keyPress);
+            UpdateDisplay();
+        }
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+
+            keyPress = Level.Direction.Down;
+            gameEngine.TriggerMovement(keyPress);
+            UpdateDisplay();
+        }
+
+        private void Right_Click(object sender, EventArgs e)
+        {
+
+            keyPress = Level.Direction.Right;
+            gameEngine.TriggerMovement(keyPress);
+            UpdateDisplay();
+        }
+
+        private void Left_Click(object sender, EventArgs e)
+        {
+
+            keyPress = Level.Direction.Left;
+            gameEngine.TriggerMovement(keyPress);
+            UpdateDisplay();
+        }
+
+    }
+}
---
+++ Game-dev-S2-project-1-master v1/Game-dev-S2-project-1-master/Game dev S2 project 1/GameEngine.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_dev_S2_project_1
+{
+    public class GameEngine
+    {
+        //NumLevels stores the number of levels in the game
+        private Level currentlevel;
+        private int NumLevels;
+        private int currentLevelNumber = 1;
+        //private random used for rolling random numbers
+
+        private Random random;
+        const int MIN_SIZE = 10;
+        const int MAX_SIZE = 20;
+
+        //Enum for tracking level state
+        private GameState game = GameState.InProgress;
+
+        public GameEngine(int NumLevels)
+        {
+            //The width and height of the level will be determined by rolling
+            //a random number between MIN_SIZE and MAX_SIZE for both the width
+            //and the height of the level
+
+            Random rnd = new Random();
+            int width = rnd.Next(MIN_SIZE, MAX_SIZE + 1);
+            int height = rnd.Next(MIN_SIZE, MAX_SIZE + 1);
+            this.NumLevels = NumLevels;
+            currentlevel = new Level(width, height);
+
+        }
+        //This method will return the ToString value of the current-level , or an end screen if the game is completed
+        public String ToString()
+        {
+            String result = "";
+            if (game == GameState.Complete)
+            {
+                result = "CONGRATULATIONS, YOU'VE FINISHED THE GAME!";
+            }
+            else if (game == GameState.InProgress) {
+                result = currentlevel.ToString();
+            } 
+            else if (game == GameState.GameOver) {
+                result = "";
+            }
+            return result;
+        }
+
+        //MoveHero method which signifies the desired move
+        private bool MoveHero(Level.Direction move)
+        {
+            
+            bool success = false;
+            int targetTile = (int)move;
+
+            HeroTile hero = currentlevel.getHeroTile();
+
+            //Checks if tile is an exit tile
+            if (hero.visionArray[targetTile].display == '▒') {
+                success = true;
+
+                if (currentLevelNumber >= NumLevels)
+                {
+                    game = GameState.Complete;
+                    success = false;
+                }
+                else {
+                    NextLevel();
+                    success = true;
+                }
+            }
+
+            //checks if tile is an empty tile
+            if (hero.visionArray[targetTile].display == '.')
+            {
+                success = true;
+                currentlevel.SwopTiles(hero.visionArray[targetTile], currentlevel.getHeroTile());
+                hero.UpdateVision(currentlevel);
+            }
+            return success;
+        }
+
+        //Calls MoveHero()
+        public void TriggerMovement(Level.Direction move)
+        {
+            //debug.Text = "Moving";
+            MoveHero(move);
+        }
+
+        
+        //Global enum for tracking progress regarding levels
+        public enum GameState { 
+        InProgress,
+        Complete,
+        GameOver,
+        }
+
+        //Loads next level while maintaining hero's attributes across levels
+        public void NextLevel() {
+            currentLevelNumber++;
+            HeroTile tempHero = currentlevel.getHeroTile();
+
+            Random rnd = new Random();
+            int width = rnd.Next(MIN_SIZE, MAX_SIZE + 1);
+            int height = rnd.Next(MIN_SIZE, MAX_SIZE + 1);
+
+            currentlevel = new Level(width, height, tempHero);
+        }
+    }
+}
